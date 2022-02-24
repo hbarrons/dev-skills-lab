ca127e60f1d538fee11eaf661777e3c5e0489c34
hbarrons <hbarrons@gmail.com>
2022-02-24 18:09:32 -0500

    complete dev-skills-lab part one and start part two

---
+++ controllers/skills.js
@@ -0,0 +1,36 @@
+import * as skillsDb from '../data/skills-db.js'
+
+function index(req, res) {
+  skillsDb.find({}, function(error, skills) {
+    res.render('skills/index.ejs', {
+      skills: skills,
+      error: error
+    })
+  })
+}
+
+function show(req, res) {
+  skillsDb.findById(req.params.id, function(error, skill){
+    res.render('skills/show', {
+      skill: skill,
+      error: error
+    })
+  })
+}
+
+function newSkill(req,res){
+  res.render('skills/new.ejs')
+}
+
+function create(req, res) {
+  skillsDb.create(req.body, function(error, todo){
+    res.redirect('/skills')
+  })
+}
+
+export {
+  index,
+  show,
+  newSkill as new,
+  create
+}
---
+++ data/skills-db.js
@@ -0,0 +1,56 @@
+const skills = [
+  {skill: 'HTML', experience: true, _id: 1062},
+  {skill: 'JavaScript', experience: true, _id: 1202},
+  {skill: 'CSS', experience: true, _id: 9184},
+  {skill: 'Python', experience: false, _id: 8122},
+  {skill: 'Java', experience: false, _id: 1312},
+  {skill: 'C', experience: false, _id: 1311},
+]
+
+const find = (conditions, callback) => {
+  try {
+    if (!(conditions instanceof Object)){
+      throw new TypeError('Please pass in an object')
+    }
+    if (Object.keys(conditions).length === 0) return callback(null, skills)
+  } catch (error) {
+    console.log(error)
+    callback(error, [])
+  }
+}
+
+const findById = (id, callback) => {
+  try {
+    const skill = skills.find(skill => skill._id === parseInt(id))
+    if (!skill) throw new Error ('No skill was found')
+    return callback(null, skill)
+  } catch (error) {
+    console.log(error)
+    return callback(error, null)
+  }
+}
+
+function create(skill, callback) {
+  skill._id = Date.now() % 100000
+  skill.done = false
+  skills.push(skill)
+  return callback(null, skill)
+}
+
+function findByIdAndDelete(id, callback) {
+  try { 
+    const idx = skills.findIndex(skill => skill._id == parseInt(id))
+    const deletedSkill = skills.splice(idx, 1)
+    if (!deletedSkill.length ) throw new Error ('No skill was deleted')
+    return callback(null, deletedSkill[0])
+  } catch(error) {
+    return callback(error, null)
+  }
+}
+
+export { 
+	find,
+  findById,
+  create,
+  findByIdAndDelete
+}
